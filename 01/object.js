f926098f94c16b891e9542e0badfbefcff19ba15
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-05-06 22:35:49 +0530

    Add the object js file

---
+++ 01/object.js
@@ -0,0 +1,29 @@
+// let obj = {
+//   name: "Aniruddha Basak",
+//   college: "BPPIMT",
+//   place: "Birati",
+//   stream: "CSE"
+// }
+
+// console.log(obj.name);
+
+// console.log(obj.place);
+// obj.place = "Kolkata"
+// console.log(obj.place);
+
+// var Object = function (name, college, place, stream) {
+//   this.firstName = name;
+//   this.college = college;
+//   this.place = place;
+//   this.stream = stream;
+//   this.getData = function() {
+//     console.log(`${this.firstName} studies in ${this.college} in ${this.stream} branch.`);
+//   }
+// };
+
+// let ani = new Object("Aniruddha", "BPPIMT", "Birati", "CSE")
+// let aditi = new Object("Aditi", "BPPIMT", "Chandannagar", "CSE")
+// // console.log(ani);
+// // console.log(aditi);
+// ani.getData()
+// aditi.getData()
